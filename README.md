5e9a481ae4729046b26c06d607639f9b7c954e7d
Vladyslav Beliaiev <vbeliaiev@makemusic.com>
2021-09-03 14:43:22 +0200

    Add README, modify the syntax of CustomsCarCalculator

---
+++ README.md
@@ -0,0 +1,5 @@
+This is a simple customs car calculator.
+
+The purpose of this app is to determine the costs to transfer a car from EU to Ukraine
+
+Use `rackup -p 4567` to run the server locally
--- customs_car_calculator.rb
+++ customs_car_calculator.rb
@@ -3,14 +3,13 @@ require 'sinatra/asset_pipeline'
 
 class CustomsCarCalculator < Sinatra::Base
   register Sinatra::AssetPipeline
-  set :assets_paths, %w(assets)
 
-  set :assets_precompile, %w(application.css)
+  set :assets_precompile, %w[application.css]
+
   configure do
-    # Setup Sprockets
-    sprockets.append_path File.join(root, 'assets', 'stylesheets')
-    sprockets.append_path File.join(root, 'assets', 'javascripts')
-    sprockets.append_path File.join(root, 'assets', 'images')
+    %w[stylesheets javascripts images].each do |assets_dir|
+      sprockets.append_path File.join(root, 'assets', assets_dir)
+    end
     sprockets.append_path File.join(root, 'vendor', 'assets', 'bootstrap5')
   end
 
@@ -22,4 +21,3 @@ class CustomsCarCalculator < Sinatra::Base
   
   run! if app_file == $0
 end
-
